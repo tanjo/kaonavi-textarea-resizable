84f41cd52737bbbafbe0c972a22e2b87b2e2406c
tanjo <tanjo@users.noreply.github.com>
2021-10-22 17:23:55 +0900

    修正

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
-windows.onload = () => {
-  [...document.querySelectorAll('.js-popup-textarea-open')].forEach((e) => e.remove());
-}
+window.addEventListener("load",() => {
+
+  const observer = new MutationObserver((mutations) => {
+    [...document.querySelectorAll('.js-popup-textarea-open')].forEach((e) => e.remove());
+  });
+  if (document.getElementById('window_loading')) {
+    const config = { attributes: true, childList: true, characterData: true, subtree: true };
+    observer.observe(document.getElementById('window_loading'), config);
+    console.log('kaonavi textarea resizable!');
+  }
+});
